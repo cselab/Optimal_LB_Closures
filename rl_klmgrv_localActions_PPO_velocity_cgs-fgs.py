bc38ba36b6811658bd6605858d142d32b9e99728
paufisch <paufisch@student.ethz.ch>
2024-08-09 10:45:37 -0400

    new file to run cgs-fgs training

---
+++ rl_klmgrv_localActions_PPO_velocity_cgs-fgs.py
@@ -0,0 +1,217 @@
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+import os
+import json
+from functools import partial
+import sys
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import pickle
+from time import strftime
+ 
+from tianshou.utils import WandbLogger
+from tianshou.data import Batch, Collector, ReplayBuffer, VectorReplayBuffer
+from tianshou.env import DummyVectorEnv
+from tianshou.policy import BasePolicy, PPOPolicy, PGPolicy, A2CPolicy
+from tianshou.trainer import OnpolicyTrainer
+from tianshou.utils.net.common import ActorCritic, Net
+from tianshou.utils.net.continuous import Actor, Critic, ActorProb
+from tianshou.trainer.utils import gather_info, test_episode
+
+import gymnasium as gym
+from gymnasium import spaces
+from gymnasium.wrappers import TimeLimit, RescaleAction, TransformObservation
+from stable_baselines3.common.env_checker import check_env
+
+from lib.environments import *
+from lib.policy import get_rl_algo
+from lib.distributions import ElementwiseNormal
+from lib.models import get_actor_critic
+from lib.utils import restrict_to_num_threads, save_batch_to_file, save_dict_to_file, model_name
+from lib.trainer import MyOnpolicyTrainer
+from lib.models import *
+from lib.custom_tianshou.my_logger import WandbLogger2
+
+#temporary solution for xlb imports
+sys.path.append(os.path.abspath('/home/pfischer/XLB'))
+#from my_flows.kolmogorov_2d import Kolmogorov_flow
+from my_flows.helpers import get_kwargs
+
+#from lib.custom_tianshou.my_actors import MyActorProb
+
+import wandb
+wandb.require("core")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--algorithm", type=str, default="ppo")
+    parser.add_argument("--environment", type=str, default="Kolmogorov6")
+
+    parser.add_argument("--seed", type=int, default=0)
+
+    #ENVIRONMENT ARGUMENTS 
+    parser.add_argument("--step_factor", type=int, default=2)
+    parser.add_argument("--cgs_resolution", type=int, default=1)    
+    parser.add_argument("--fgs_resolution", type=int, default=16)
+    parser.add_argument("--max_interactions", type=int, default=1536-1)
+    parser.add_argument("--train_num", type=int, default=1)
+    parser.add_argument("--test_num", type=int, default=1)
+
+    #POLICY ARGUMENTS 
+    parser.add_argument("--learning_rate", type=float, default=3e-4)
+    parser.add_argument("--adam_eps", type=float, default=1e-7)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--reward_normalization", type=bool, default=True) 
+    parser.add_argument("--deterministic_eval", type=bool, default=True)
+    parser.add_argument("--action_scaling", type=bool, default=True)
+    parser.add_argument("--action_bound_method", type=str, default="tanh")
+    parser.add_argument("--ent_coef", type=float, default=-1e-4)
+    parser.add_argument("--max_grad_norm", type=float, default=0.5)
+    parser.add_argument("--gae_lambda", type=float, default=0.9) 
+
+    #COLLECTOR ARGUMENTS
+    parser.add_argument("--buffer_size", type=int, default=20000)
+
+    #LOGGER ARGUMENTS
+    parser.add_argument("--logdir", type=str, default="log")
+    parser.add_argument("--task", type=str, default="local-omega-learning")
+    
+    #TRAINER ARGUMENTS
+    parser.add_argument("--max_epoch", type=int, default=10)
+    parser.add_argument("--step_per_epoch", type=int, default=1536-1)
+    parser.add_argument("--repeat_per_collect", type=int, default=3)
+    parser.add_argument("--episode_per_test", type=int, default=2)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--step_per_collect", type=int, default=100)
+    parser.add_argument("--episode_per_collect", type=int, default=1)
+    parser.add_argument("--reward_threshold", type=int, default=-5e-4)
+
+    return parser.parse_known_args()[0]
+
+
+
+if __name__ == '__main__':
+
+    #######################################################################################################
+    ####### setup stuff *##################################################################################
+    #######################################################################################################
+    args = get_args()
+    print(args)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    dump_dir = model_name(args)
+
+    
+
+    #######################################################################################################
+    ####### environments ##################################################################################
+    #######################################################################################################
+    seeds = np.array([102, 348, 270, 106, 71, 188, 20, 121, 214, 330, 87, 372,
+                  99, 359, 151, 130, 149, 308, 257, 343, 413, 293, 385, 191, 276,
+                  160, 313, 21, 252, 235, 344, 42])
+
+    assert seeds.shape[0] == np.unique(seeds).shape[0]
+    train_seeds = seeds[:30]
+    val_seeds = seeds[30:]
+    #test_seeds = np.array([69, 33, 420])
+    
+    train_env = KolmogorovEnvironment7(seeds=train_seeds, max_episode_steps=args.max_interactions, step_factor=args.step_factor)
+    test_env = KolmogorovEnvironment7(seeds=val_seeds, max_episode_steps=args.max_interactions, step_factor=args.step_factor)
+    #######################################################################################################
+    ####### Policy ########################################################################################
+    #######################################################################################################
+    assert train_env.observation_space.shape is not None  # for mypy
+    assert train_env.action_space.shape is not None
+    #initialize PPO
+    actor = MyFCNNActorProb(in_channels=2, device=device).to(device)
+    critic_backbone = Backbone(in_channels=2, device=device).to(device)
+    critic = Critic(preprocess_net=critic_backbone, preprocess_net_output_dim=64, device=device).to(device)
+    optim = torch.optim.Adam(actor.parameters(), lr=args.learning_rate, eps=args.adam_eps)
+    dist = torch.distributions.Normal
+    policy = PPOPolicy(actor=actor,
+        critic=critic, 
+        optim=optim,
+        dist_fn=dist, 
+        action_space=train_env.action_space,
+        discount_factor=args.gamma,
+        reward_normalization=args.reward_normalization, 
+        deterministic_eval=args.deterministic_eval,
+        action_scaling=args.action_scaling,
+        action_bound_method=args.action_bound_method,
+        ent_coef = args.ent_coef,
+        max_grad_norm = args.max_grad_norm,
+        gae_lambda=args.gae_lambda, 
+    )
+
+    #######################################################################################################
+    ####### Collectors ####################################################################################
+    #######################################################################################################
+    #train_collector = Collector(policy=policy, env=train_env, buffer=VectorReplayBuffer(args.buffer_size, len(train_env)))
+    train_collector = Collector(policy=policy, env=train_env, buffer=VectorReplayBuffer(args.buffer_size, 1))
+    test_collector = Collector(policy=policy, env=test_env)
+    train_collector.reset()
+    test_collector.reset()
+
+    #######################################################################################################
+    ####### Logger ########################################################################################
+    #######################################################################################################
+    log_path = os.path.join(args.logdir, args.task, "ppo")
+    logger = WandbLogger2(config=args, train_interval=100, update_interval=100,
+                             test_interval=1, info_interval=1)
+    writer = SummaryWriter(log_path)
+    writer.add_text("args", str(args))
+    logger.load(writer)
+
+    #######################################################################################################
+    ####### Trainer #######################################################################################
+    #######################################################################################################
+    trainer = OnpolicyTrainer(
+        policy=policy,
+        train_collector=train_collector,
+        test_collector=test_collector,
+        max_epoch=args.max_epoch,
+        step_per_epoch=args.step_per_epoch,
+        repeat_per_collect=args.repeat_per_collect,
+        episode_per_test=args.episode_per_test,
+        batch_size=args.batch_size,
+        step_per_collect=args.step_per_collect,
+        #episode_per_collect=args.episode_per_collect,
+        show_progress=True,
+        logger=logger,
+        #stop_fn=lambda mean_reward: mean_reward >= args.reward_threshold,
+    )
+    
+    #######################################################################################################
+    #######  run training  ################################################################################
+    #######################################################################################################
+    epoch_results = []
+    for _,epoch_stats,_ in trainer:
+        epoch_results.append(Batch(epoch_stats))
+ 
+    # stack totoal results
+    total_results = Batch.stack(epoch_results)
+
+    # Generate a unique ID based on the current timestamp
+    unique_id = strftime("%Y%m%d-%H%M%S")
+
+    # Save total results
+    total_results_fname = f"{dump_dir}/training_stats_{unique_id}.pkl"
+    save_batch_to_file(total_results, total_results_fname)
+
+    # Save config file
+    config_fname = f"{dump_dir}/config_{unique_id}.pkl"
+    save_batch_to_file(args, config_fname)
+
+    # Save policy
+    policy_fname = f"{dump_dir}/policy_{unique_id}.pth"
+    torch.save(policy.state_dict(), policy_fname)
+
+ 
